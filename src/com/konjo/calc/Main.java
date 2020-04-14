836d7614a65351e8f9566b0d5e3ec33a42dd15ac
Wesley Scholl <clients@konjoweb.com>
2020-04-14 14:50:33 -0400

    added getters and setters aka accessors and mutators

--- src/com/konjo/calc/Main.java
+++ src/com/konjo/calc/Main.java
@@ -12,16 +12,16 @@ public class Main {
 
         for(MathEquation equation : equations) {
             equation.execute();
-            System.out.println(equation.result);
+            System.out.println(equation.getResult());
         }
 
     }
 
     public static MathEquation create(double leftVal, double rightVal, char opCode) {
         MathEquation equation = new MathEquation();
-        equation.leftVal = leftVal;
-        equation.rightVal = rightVal;
-        equation.opCode = opCode;
+        equation.setLeftVal(leftVal);
+        equation.setRightVal(rightVal);
+        equation.setOpCode(opCode);
 
         return equation;
     }
--- src/com/konjo/calc/MathEquation.java
+++ src/com/konjo/calc/MathEquation.java
@@ -1,10 +1,19 @@
 package com.konjo.calc;
 
 public class MathEquation {
-    public double leftVal;
-    public double rightVal;
-    public char opCode;
-    public double result;
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
+
+    public double getLeftVal() {return leftVal;}
+    public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
+    public double getRightVal() {return rightVal;}
+    public void setRightVal(double rightVal) {this.rightVal = rightVal;}
+    public double getOpCode() {return opCode;}
+    public void setOpCode(char opCode) {this.opCode = opCode;}
+
+    public double getResult() {return result;}
 
 public void  execute() {
     switch (opCode) {
