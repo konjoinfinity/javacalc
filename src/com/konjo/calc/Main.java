b9247dce223375ff95db09b2b0ca4cdcba474d8b
Wesley Scholl <clients@konjoweb.com>
2020-04-14 12:12:17 -0400

    new java calculator app

---
+++ src/com/konjo/calc/Main.java
@@ -0,0 +1,40 @@
+package com.konjo.calc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double[] leftVals = { 100.0d, 25.0d, 225.0d, 11.0d };
+        double[] rightVals = { 50.0d, 92.0d, 17.0d, 3.0d };
+        char[] opCodes = { 'd', 'a', 's', 'm' };
+        double[] results = new double[opCodes.length];
+//        double val1 = 100.0d;
+//        double val2 = 0.0d;
+//        double result;
+//        char opCode = 'd';
+
+        for(int i = 0; i < opCodes.length; i++) {
+            switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+                    break;
+                default:
+                    System.out.println("Error - invalid operator");
+                    results[i] = 0.0d;
+                    break;
+            }
+        }
+        for(double theResult : results) {
+            System.out.println("The result: " + theResult);
+        }
+
+    }
+}
