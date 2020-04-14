edc10e4a18d4ef4a8f9fb957a37c83b638332428
Wesley Scholl <clients@konjoweb.com>
2020-04-14 15:46:10 -0400

    replaced the public static method and replaced with constructors

--- src/com/konjo/calc/Main.java
+++ src/com/konjo/calc/Main.java
@@ -4,11 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        MathEquation testEquation = new MathEquation();
+//        testEquation.execute();
+//        System.out.println("Test = ");
+//        System.out.println(testEquation.getResult());
+
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = create(100.0d, 50.0d, 'd');
-        equations[1] = create(25.0d, 92.0d, 'a');
-        equations[2] = create(225.0d, 17.0d, 's');
-        equations[3] = create(11.0d, 3.0d, 'm');
+        equations[0] = new MathEquation('d', 100.0d, 50.0d );
+        equations[1] = new MathEquation('a',25.0d, 92.0d);
+        equations[2] = new MathEquation( 's',225.0d, 17.0d);
+        equations[3] = new MathEquation('m',11.0d, 3.0d);
 
         for(MathEquation equation : equations) {
             equation.execute();
@@ -17,12 +22,12 @@ public class Main {
 
     }
 
-    public static MathEquation create(double leftVal, double rightVal, char opCode) {
-        MathEquation equation = new MathEquation();
-        equation.setLeftVal(leftVal);
-        equation.setRightVal(rightVal);
-        equation.setOpCode(opCode);
-
-        return equation;
-    }
+//    public static MathEquation create(double leftVal, double rightVal, char opCode) {
+//        MathEquation equation = new MathEquation();
+//        equation.setLeftVal(leftVal);
+//        equation.setRightVal(rightVal);
+//        equation.setOpCode(opCode);
+//
+//        return equation;
+//    }
 }
--- src/com/konjo/calc/MathEquation.java
+++ src/com/konjo/calc/MathEquation.java
@@ -3,7 +3,7 @@ package com.konjo.calc;
 public class MathEquation {
     private double leftVal;
     private double rightVal;
-    private char opCode;
+    private char opCode = 'a';
     private double result;
 
     public double getLeftVal() {return leftVal;}
@@ -14,6 +14,18 @@ public class MathEquation {
     public void setOpCode(char opCode) {this.opCode = opCode;}
 
     public double getResult() {return result;}
+
+    public MathEquation() {};
+
+    public MathEquation(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
 
 public void  execute() {
     switch (opCode) {
