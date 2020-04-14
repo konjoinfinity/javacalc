e8001e62e03df08f9f36095d2b21c36ad60c668f
Wesley Scholl <clients@konjoweb.com>
2020-04-14 12:14:55 -0400

    removed commented code

--- src/com/konjo/calc/Main.java
+++ src/com/konjo/calc/Main.java
@@ -7,10 +7,6 @@ public class Main {
         double[] rightVals = { 50.0d, 92.0d, 17.0d, 3.0d };
         char[] opCodes = { 'd', 'a', 's', 'm' };
         double[] results = new double[opCodes.length];
-//        double val1 = 100.0d;
-//        double val2 = 0.0d;
-//        double result;
-//        char opCode = 'd';
 
         for(int i = 0; i < opCodes.length; i++) {
             switch (opCodes[i]) {
