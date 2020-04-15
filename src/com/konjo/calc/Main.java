5c14d8cfc60a8dbaf145c315587e592dded84054
Wesley Scholl <clients@konjoweb.com>
2020-04-15 11:21:18 -0400

    updated with double and int handling using overloads

--- src/com/konjo/calc/Main.java
+++ src/com/konjo/calc/Main.java
@@ -15,5 +15,24 @@ public class Main {
             System.out.println(equation.getResult());
         }
 
+        System.out.println();
+        System.out.println("Using Overloads");
+        System.out.println();
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+        int leftInt = 9;
+        int rightInt = 4;
+
+        MathEquation equationOverload = new MathEquation('d');
+
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.println("result = " + equationOverload.getResult());
+
+        equationOverload.execute(leftInt, rightInt);
+        System.out.println("result = " + equationOverload.getResult());
+
+        equationOverload.execute((double)leftInt, rightInt);
+        System.out.println("result = " + equationOverload.getResult());
     }
 }
